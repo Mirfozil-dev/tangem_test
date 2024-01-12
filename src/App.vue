3e7eb14e7955c50f553effdb7af5930c8f77176b
Robenson Jack <robensonjack@Robensons-MacBook-Pro.local>
2024-01-12 17:23:56 +0200

    added some functionality

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
 <script setup>
 import NavbarAlert from './components/NavbarAlert.vue';
 import CardAlert from './components/CardAlert.vue';
+import { ref, onMounted, onUnmounted, computed, nextTick, watch } from 'vue';
+
+const scrolled = ref(false)
+const isLoading = ref(true)
+const cardToBeHidden = ref(false)
+
+const onScroll = () => {
+  if (window.scrollY > 107) {
+    scrolled.value = true
+  }
+}
+
+const hideCard = () => {
+  cardToBeHidden.value = true
+  localStorage.setItem('cardToBeHidden', 'true')
+}
+
+const showCard = computed(() => !isLoading.value && scrolled.value && !cardToBeHidden.value)
+
+onMounted(async () => {
+  await nextTick()
+  cardToBeHidden.value = localStorage.getItem('cardToBeHidden')
+  isLoading.value = false
+  window.addEventListener('scroll', onScroll)
+})
+onUnmounted(() => {
+  window.removeEventListener('scroll', onScroll)
+})
+
 </script>
 
 <template>
   <main>
-    <NavbarAlert />
-    <CardAlert />
+    <NavbarAlert/>
+    <CardAlert :showCard="showCard" @hideCard="hideCard" />
   </main>
 </template>
--- src/components/CardAlert.vue
+++ src/components/CardAlert.vue
@@ -1,6 +1,27 @@
+<script setup lang="ts">
+import { emit } from 'process';
+import { defineProps, defineEmits, watch, computed } from 'vue';
+
+type Props = {
+  showCard: boolean
+}
+
+const props = defineProps<Props>();
+const emit = defineEmits(['hideCard'])
+
+const showCard = computed(() => props.showCard)
+
+
+const hideCard = () => {
+  emit('hideCard')
+}
+
+</script>
+
 <template>
-    <div class="card-alert">
+    <div class="card-alert" :class="{ 'card-alert--show': showCard }">
       <img
+        @click="hideCard()"
         loading="lazy"
         style="z-index: 999; cursor: pointer;"
         src="../assets/images/close_icon.svg"
